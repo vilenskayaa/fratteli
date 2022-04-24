e48ffd45b66d677cca34fd558ca28f415dc92c86
Dmitry Yegorov <dmitry.yegorov@effective-soft.com>
2022-04-24 15:30:28 +0300

    added get list of all test if test_id is missing

--- app/test/get-test.php
+++ app/test/get-test.php
@@ -9,36 +9,48 @@ header("Content-Type: application/json;");
 
 $test_id = $_GET["test_id"];
 
-$selectTest = "SELECT * FROM `test` WHERE `test_id` = '$test_id'";
-$selectQuestions = "SELECT * FROM `question` WHERE `test_id` = '$test_id'";
-
-$testData = $db->query($selectTest)->fetch_assoc();
-$questionsData = $db->query($selectQuestions);
-
-$res = array(
-    "test_id" => $testData["test_id"],
-    "test_title" => $testData["test_title"],
-    "test_time" => $testData["test_time"],
-    "test_complexity" => $testData["test_complexity"],
-    "test_level" => $testData["test_level"],
-    "questions" => []
-);
-
-while ($question = $questionsData->fetch_assoc()) {
-    $question_id = $question["question_id"];
-    $selectAnswer = "SELECT * FROM `answer` WHERE `question_id` = '$question_id'";
-    $answersData = $db->query($selectAnswer);
-
-    $question["answers"] = array();
-
-    while ($answer = $answersData->fetch_assoc()) {
-        $answer["is_correct"] = (bool)$answer["is_correct"];
-        array_push($question["answers"], $answer);
+if ($test_id) {
+    $selectTest = "SELECT * FROM `test` WHERE `test_id` = '$test_id'";
+    $selectQuestions = "SELECT * FROM `question` WHERE `test_id` = '$test_id'";
+    
+    $testData = $db->query($selectTest)->fetch_assoc();
+    $questionsData = $db->query($selectQuestions);
+    
+    $res = array(
+        "test_id" => $testData["test_id"],
+        "test_title" => $testData["test_title"],
+        "test_time" => $testData["test_time"],
+        "test_complexity" => $testData["test_complexity"],
+        "test_level" => $testData["test_level"],
+        "questions" => []
+    );
+    
+    while ($question = $questionsData->fetch_assoc()) {
+        $question_id = $question["question_id"];
+        $selectAnswer = "SELECT * FROM `answer` WHERE `question_id` = '$question_id'";
+        $answersData = $db->query($selectAnswer);
+    
+        $question["answers"] = array();
+    
+        while ($answer = $answersData->fetch_assoc()) {
+            $answer["is_correct"] = (bool)$answer["is_correct"];
+            array_push($question["answers"], $answer);
+        }
+    
+        array_push($res["questions"], $question);
+    
     }
+} else {
+    $selectTest = "SELECT * FROM `test`";
+    $testsList = $db->query($selectTest);
 
-    array_push($res["questions"], $question);
+    $res = [];
 
+    while ($test = $testsList->fetch_assoc()) {
+        array_push($res, $test);
+    }
 }
+
 
 
 echo json_encode($res, JSON_UNESCAPED_UNICODE);
