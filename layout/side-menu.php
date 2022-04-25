115966c105e25ef35801b6b3b19a55703b3a4306
Dmitry Yegorov <dmitry.yegorov@effective-soft.com>
2022-04-26 01:05:16 +0300

    added href paths to menu items

--- layout/side-menu.php
+++ layout/side-menu.php
@@ -5,10 +5,9 @@
       </div>
       <nav class="nav__list">
         <a href="" class="nav__item">Мое расписание</a>
-        <a href="" class="nav__item">Мои группы</a>
+        <a href="../web/group-list.php" class="nav__item">Мои группы</a>
         <a href="" class="nav__item">Словарь</a>
-        <a href="" class="nav__item">Словарь</a>
-        <a href="" class="nav__item">Тесты</a>
+        <a href="../web/quiz-list.php" class="nav__item">Тесты</a>
       </nav>
     </div>
     <div class="nav__bottom">
