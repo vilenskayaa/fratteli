182b7737e39de8131c57efc2e21b4fbc2fcd6c03
Dmitry Yegorov <dmitry.yegorov@effective-soft.com>
2022-04-26 01:02:38 +0300

    separated menu

--- web/quiz.php
+++ web/quiz.php
@@ -9,24 +9,8 @@ $page_title = "Тест";
 <?php include "../layout/meta.php"; ?>
 
 <body class="container__aside">
-  <aside class="nav">
-    <div class="nav__top">
-      <div class="nav__logo">
-        <img src="/assets/img/logo.png" alt="">
-      </div>
-      <nav class="nav__list">
-        <a href="" class="nav__item">Мое расписание</a>
-        <a href="" class="nav__item">Мои группы</a>
-        <a href="" class="nav__item">Словарь</a>
-        <a href="" class="nav__item">Словарь</a>
-        <a href="/quiz-list.php" class="nav__item">Тесты</a>
-      </nav>
-    </div>
-    <div class="nav__bottom">
-      <a href="" class="nav__bottom-item">Настройки</a>
-      <a href="/app/account/logout.php" class="nav__bottom-item">Выход</a>
-    </div>
-  </aside>
+  
+<?php include "../layout/side-menu.php" ?>
   <main class="">
       <div id="testContainer"></div>
   </main>
