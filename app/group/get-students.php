bca002a1e99040a1c6f6b5b74b6b3fcd24bf9e66
vsguk <vsguk2013@gmail.com>
2022-05-28 15:54:48 +0300

    Revert "Get posts"
    This reverts commit 722b7a948c935dff6d707a2d837214c58ea3ea3d.

--- app/group/get-students.php
+++ app/group/get-students.php
@@ -7,21 +7,21 @@ header("Content-Type: application/json;");
 
 try {
 
-    $json = file_get_contents('php://input');
-    $req = json_decode($json, true);
+  $json = file_get_contents('php://input');
+  $req = json_decode($json, true);
 
-    $group_id = $_GET['id'];
+  $group_id = $_GET['id'];
 
-    $res = array();
+  $res = array();
 
-    $selectStudents = "SELECT * FROM `student` INNER JOIN user ON student.user_id=user.user_id WHERE `group_id` = '$group_id';";
+  $selectStudents = "SELECT * FROM `student` INNER JOIN user ON student.user_id=user.user_id WHERE `group_id` = '$group_id';";
 
-    $groupsData = $db->query($selectStudents);
-    while ($group = $groupsData->fetch_assoc()) {
-        array_push($res, $group);
-    }
+  $groupsData = $db->query($selectStudents);
+  while ($group = $groupsData->fetch_assoc()) {
+    array_push($res, $group);
+  }
 
-    echo json_encode($res);
+  echo json_encode($res);
 } catch (Exception $e) {
-    echo json_encode(array("error" => $e->getMessage(), "success" => false));
+  echo json_encode(array("error" => $e->getMessage(), "success" => false));
 }
