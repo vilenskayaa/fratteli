8e9e28fc929c82b6483c0ed3e0f6eee12e16d750
vilenskayaa <a.vilenskaya@icloud.com>
2022-06-03 02:13:30 +0300

    Update reg-test.php

--- web/reg-test.php
+++ web/reg-test.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/styles/css/reg-test.css">
-    <title>Document</title>
+    <title>Определение уровня итальянского</title>
 </head>
 
 <body>
